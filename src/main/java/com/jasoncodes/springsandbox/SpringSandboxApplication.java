41cad62d69bd1d13ce8e4eeee8e28889fce299d4
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-25 21:37:27 -0500

    start spring sandbox (again)

---
+++ src/main/java/com/jasoncodes/springsandbox/SpringSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.jasoncodes.springsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSandboxApplication.class, args);
+    }
+
+}
