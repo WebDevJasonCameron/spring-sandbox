91c1eb21bf27a5ac8e1525595ebae217440dfa51
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-26 17:41:14 -0500

    del all test files. starting over with js api retrieval

--- src/main/java/com/jasoncodes/springsandbox/Checks/EmployeeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.jasoncodes.springsandbox.Checks;
-
-public class EmployeeNotFoundException extends RuntimeException {
-
-    public EmployeeNotFoundException(Long id) {
-        super("Could not find employee " + id);
-    }
-}
--- src/main/java/com/jasoncodes/springsandbox/Repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jasoncodes.springsandbox.Repositories;
-
-import com.jasoncodes.springsandbox.Models.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
