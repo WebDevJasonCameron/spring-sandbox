2816611973a65fd51d1ad93d6abffc70ac10c114
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-26 11:31:13 -0500

    working api for spoon

---
+++ src/main/java/com/jasoncodes/springsandbox/Controllers/RestSpringBootController.java
@@ -0,0 +1,56 @@
+package com.jasoncodes.springsandbox.Controllers;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class RestSpringBootController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+    @GetMapping(value = "/callclienthello")
+    private String getHelloClient(){
+        String url = "http://localhost:8080/hello";                             //   API Address
+                                                                                // Will add key
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(url, String.class);           //   Combine all for URL
+        return result;
+    }
+
+    @GetMapping(value = "/people")
+    @JsonBackReference
+    private String getPeople(){
+        String url = "https://swapi.dev/api/people";
+        // Will add key
+        RestTemplate restTemplate = new RestTemplate();
+        String people = restTemplate.getForObject(url, String.class);
+        return people;
+    }
+
+    @GetMapping(value = "/p1")
+    @JsonBackReference
+    private String getPersonIndex1(){
+        String url = "https://swapi.dev/api/people/1";
+        // Will add key
+        RestTemplate restTemplate = new RestTemplate();
+        String people = restTemplate.getForObject(url, String.class);
+        return people;
+    }
+
+    @GetMapping(value = "/r1")
+    @JsonBackReference
+    private String getRecipe(){
+        String url = "https://api.spoonacular.com/recipes/complexSearchquery=pasta";
+        // Will add key
+        RestTemplate restTemplate = new RestTemplate();
+        String r1 = restTemplate.getForObject(url, String.class);
+        return r1;
+    }
+
+}
