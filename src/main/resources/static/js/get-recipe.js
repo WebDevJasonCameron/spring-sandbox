d7bf34fcbaf64997c437e4f4b7f13b9c4d42b441
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-28 15:46:00 -0500

    have list of r showing. conv t into card

--- src/main/resources/static/js/get-recipe.js
+++ src/main/resources/static/js/get-recipe.js
@@ -2,20 +2,25 @@
 let rLR;
 
 
-// GET RN
+// SEARCH BTN ACTION
 $('#get-recipe-btn').on('click', (e) => {
     e.preventDefault();
-    readSpoonRecipeListByKeyWord($('#search-input').val())
+    getSpoonRecipeListByKeyWord($('#search-input').val())
 } )
 
+// GOT TO DETAILS PAGE
+$('.add-btn').on('click', (e) => {
+    e.preventDefault();
+    getSpoonRecipeDetailsByID(id);
+})
 
-// READ
-function readSpoonRecipeListByKeyWord(kw){
+// GET R LIST
+function getSpoonRecipeListByKeyWord(kw){
     const apiKey01 = SPOON_KEY_01;
     const apiKey02 = SPOON_KEY_02;
     const apiKey03 = SPOON_KEY_03;
 
-    const spoonURL = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey01 + '&query=' + kw;
+    const spoonURL = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=' + apiKey01 + '&query=' + kw + '&offset=0&number=100';
     const readOption = {
         method: 'GET',
     };
@@ -24,37 +29,80 @@ function readSpoonRecipeListByKeyWord(kw){
         .then((res) => res.json())
         .then((data) => {
             console.log(data);
-            console.log(data.results[0].id)
             rLR = data;
-            console.log(rLR.results[0].id);
-        // }).then(() => {
-            console.log(rLR.results[0].id);
-            // cycleDataArray(recipeListReturn);
+        }).then(() => {
+            $('#recipe-results').html(combineRows(rLR));
     })
-
 }
 
-// TEST
-function cycleDataArray(dArray){
-    for (let i = 0; i < dArray.length; i++) {
-        console.log(dArray.results[i] + ' | ' + dArray.results[i].title + ' | ' + dArray.results[i].image);
+// GET R DETAILS
+function getSpoonRecipeDetailsByID(cid){
+    const apiKey01 = SPOON_KEY_01;
+    const apiKey02 = SPOON_KEY_02;
+    const apiKey03 = SPOON_KEY_03;
 
+    const spoonURL = 'https://api.spoonacular.com/recipes/' + cid + 'information?apiKey=' + apiKey01;
+    const readOption = {
+        method: 'GET',
+    };
+
+    fetch(spoonURL, readOption)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            rLR = data;
+        }).then(() => {
+        $('#recipe-results').html(detailsCard(rLR));
+    })
+}
+
+
+
+
+/**
+ * Body
+ */
+// CREATE CARD
+function makeRow(r){
+    const out =
+        `
+        <div class="card border-0 mt-4">
+            <div class="row no-gutters">
+                <div class="col-sm-3">
+                    <img src="${r.image}"" alt="#" class="card-img-top">
+                </div>
+                <div class="col-sm-9">
+                    <div class="card-body">
+                        <div class="card-title">
+                            ${r.title}
+                        </div>
+                        <div class="h4">
+                             ${r.id}
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <form action="/recipes/get-list" method="get">
+                            <input type="hidden" name="recipe-to-db" value="${r.id}" >
+                            <button type="submit" class="btn btn-primary add-btn">Add..
+                            </button>  
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    console.log(out);
+    return out;
+}
+
+// COMBINE CARDS
+function combineRows(rL){
+    let output = '';
+    for (let i = 0; i < rL.results.length; i++) {
+        console.log("in the for loop")
+        output += makeRow(rL.results[i])
     }
+    console.log("output was : " + output);
+    return output;
 }
 
-
-// CREATE ROW
-function makeRow(r, n){
-    return `<tr>
-                <th scope="row">${r.results[n].image}</th>
-                <th>${r.results[n].id}</th>
-                <th>${r.results[n].title}</th>
-                <th>
-                    <form action="">
-                        <input type="hidden" name="recipe-${r.results[n].id}" value="${r.results[n].id}">
-                        <button type="submit" class="btn btn-primary">Add...</button></th>
-                    </form>
-            </tr>`
-
-
-}
